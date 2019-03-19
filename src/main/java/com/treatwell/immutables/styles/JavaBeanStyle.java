438e38485840e3e0a3b109cbb5eabf56ea1f8d29
Tristan Deloche <tristan.deloche@treatwell.com>
2019-03-19 11:38:10 +0000

    FOSS-8: Document JavaBeanStyle

--- src/main/java/com/treatwell/immutables/styles/JavaBeanStyle.java
+++ src/main/java/com/treatwell/immutables/styles/JavaBeanStyle.java
@@ -1,22 +1,44 @@
 package com.treatwell.immutables.styles;
 
+import org.immutables.value.Value.Modifiable;
 import org.immutables.value.Value.Style;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 /**
- * Style to be used in combination with {@link org.immutables.value.Value.Modifiable}. Generated
- * classes with have setters with void return types. Primary use case is for command objects
- * that would be posted using JSF.
+ * This {@link Style} is used when we require Java Beans-like support for frameworks delegation (Hibernate & co for example).
+ * <p>
+ * Immutables' default style, when combined with their {@link Modifiable} (here always always) generates an additional subclass that can be converted to
+ * and from standard immutable implementation to have support for setters. Unfortunately, the default style does not guarantee that bean-like methods will be
+ * matched as they might not have standard name and {@code void} return type.
+ * This style guarantees it.
+ * <p>
+ * For example, a class like:
+ * <pre>{@code
+ *  @Immutable
+ * @JavaBeanStyle
+ *  interface MyBean {
+ *      String getValue();
+ *  }
+ * }</pre>
+ * <p>
+ * Will generate both a standard immutable implementation usable with:
+ * <pre>{@code ImmutableMyBean.builder().value("the value").build();}</pre>
+ * <p>
+ * And a {@code ModifiableMyBean} implementation:
+ * <pre>{@code
+ *  ModifiableMyBean modifiableInstance = ModifiableMyBean.create();
+ *  modifiableInstance.setValue("the value"); // returns type is void here
+ *
+ *  ImmutableMyBean immutableInstance = modifiableInstance.toImmutable(); // can convert to immutable when done with it
+ *  ModifiableMyBean backToModifiable = ModifiableMyBean.create().from(immutableInstance); // or change you mind and do more later on
+ * }</pre>
+ *
+ * @apiNote Usage of this style should always be in combination with {@link Modifiable}.
  */
-@Style(
-        // Generate Java bean like setters (void return type)
-        beanFriendlyModifiables = true,
-        // We will let Jackson work its normal magic to compute property names, which also provides us the flexibility
-        // to go and override them more easily when we want to.
-        forceJacksonPropertyNames = false
-)
-@JsonSerialize // Triggers Jackson integration on all users.
+@Style(beanFriendlyModifiables = true, forceJacksonPropertyNames = false)
+@JsonSerialize
 public @interface JavaBeanStyle {
 
 }
+
